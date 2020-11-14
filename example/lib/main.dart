3420368d6cc0ace7bc9ad59dd715b88954785619
Dominik Spicher <dominikspicher@gmail.com>
2020-11-14 13:34:36 +0100

    add example app showcasing lifehash

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,58 +1,163 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
+import 'dart:convert';
 
-import 'package:flutter/services.dart';
+import 'package:crypto/crypto.dart';
+import 'package:flutter/material.dart';
 import 'package:lifehash/lifehash.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatefulWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  State<StatefulWidget> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  Widget _body;
 
   @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await Lifehash.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+  void didChangeDependencies() {
+    _body = demoGallery(context);
+    super.didChangeDependencies();
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+    return Scaffold(
+      appBar: AppBar(title: Text('Lifehash Demo')),
+      body: Center(child: _body),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Examples'),
+            ),
+            ListTile(
+              title: Text('Demo gallery'),
+              onTap: () {
+                setState(() {
+                  _body = demoGallery(context);
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('BitFlip'),
+              onTap: () {
+                setState(() {
+                  _body = flippingBits(context);
+                });
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+Widget flippingBits(BuildContext context) {
+  var bytes = sha256.convert(utf8.encode('BitFlip')).bytes;
+  List<int> flipIthBit(int index) {
+    List<int> result = List.from(bytes);
+    var bitIdx = index % 8;
+    var byteIdx = ((index - bitIdx) / 8).round();
+    result[byteIdx] = result[byteIdx] ^ (1 << (7 - bitIdx));
+    return result;
+  }
+
+  return Column(children: <Widget>[
+    Padding(
+      padding: EdgeInsets.all(10),
+      child: Text(
+        'Starting from x=sha256(utf8("BitFlip")), the i-th picture shows the lifehash of x with the i-th bit flipped',
+        textAlign: TextAlign.center,
+      ),
+    ),
+    Expanded(
+      child: lifehashGrid(256, flipIthBit),
+    )
+  ]);
+}
+
+Widget demoGallery(BuildContext context) {
+  return Column(children: <Widget>[
+    Padding(
+      padding: EdgeInsets.all(10),
+      child: Text(
+        'The i-th picture shows the lifehash of utf8(string(i))',
+        textAlign: TextAlign.center,
+      ),
+    ),
+    Expanded(
+      child: lifehashGrid(
+        256,
+        (i) => utf8.encode(i.toString()),
+      ),
+    )
+  ]);
+}
+
+Widget lifehashGrid(int count, Function ithBytes) {
+  var size = 5.0;
+  return Padding(
+    padding: EdgeInsets.only(top: 20),
+    child: GridView.count(
+      crossAxisCount: 2,
+      children: List.generate(
+          count,
+          (index) => Container(
+                height: size * 32 + 20,
+                width: size * 32,
+                child: Center(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        width: 32 * size,
+                        height: 32 * size,
+                        child: CustomPaint(
+                          painter: LifehashPainter(
+                            size,
+                            ithBytes(index),
+                          ),
+                        ),
+                      ),
+                      Text(index.toString())
+                    ],
+                  ),
+                ),
+              )),
+    ),
+  );
+}
+
+class LifehashPainter extends CustomPainter {
+  LifehashPainter(this.size, this.bytes);
+  final double size;
+  final List<int> bytes;
+
+  @override
+  void paint(Canvas canvas, Size _size) {
+    var paint = Paint()..style = PaintingStyle.fill;
+    var colors = lifehash(bytes);
+    for (var x = 0; x < 32; x++) {
+      for (var y = 0; y < 32; y++) {
+        paint.color = colors[y][x];
+        canvas.drawRect(Offset(x * size, y * size) & Size(size, size), paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
