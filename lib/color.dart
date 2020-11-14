d1de31f9461d02810a64a1f5a930c160bf9df401
Dominik Spicher <dominikspicher@gmail.com>
2020-11-14 13:34:33 +0100

    color: simplify function interfaces

--- lib/color.dart
+++ lib/color.dart
@@ -6,25 +6,21 @@ import 'package:lifehash/entropy.dart';
 
 List<Color> chooseGradient(Entropy entropy) {
   return entropy.nextBool()
-      ? (entropy.nextBool() ? chooseAnalogous(entropy) : choosetriadic(entropy))
+      ? (entropy.nextBool()
+          ? analgousGradient(entropy)
+          : triadicGradient(entropy))
       : (entropy.nextBool()
-          ? chooseComplementary(entropy)
-          : chooseMonochrome(entropy));
+          ? complementaryGradient(entropy)
+          : monochromeGradient(entropy));
 }
 
-List<Color> chooseComplementary(Entropy entropy) {
-  var spectrum = entropy.nextFrac();
-  var lighterAdvance = entropy.nextFrac();
-  var darkerAdvance = entropy.nextFrac();
+List<Color> complementaryGradient(Entropy entropy) {
+  var spectrum1 = entropy.nextFrac();
+  var lighterAdvance = entropy.nextFrac() * 0.3;
+  var darkerAdvance = entropy.nextFrac() * 0.3;
   bool isReversed = entropy.nextBool();
-  return complementaryGradient(
-      spectrum, lighterAdvance, darkerAdvance, isReversed);
-}
-
-List<Color> complementaryGradient(double spectrum, double lighterAdvance,
-    double darkerAdvance, bool isReversed) {
-  var spectrum2 = (spectrum + 0.5) % 1;
-  var color1 = spectrumColor(spectrum);
+  var spectrum2 = (spectrum1 + 0.5) % 1;
+  var color1 = spectrumColor(spectrum1);
   var color2 = spectrumColor(spectrum2);
   var darkerColor;
   var lighterColor;
@@ -35,42 +31,34 @@ List<Color> complementaryGradient(double spectrum, double lighterAdvance,
     darkerColor = color1;
     lighterColor = color2;
   }
-  var adjustedLighter = Color.lerp(lighterColor, white, lighterAdvance * 0.3);
-  var adjustedDarker = Color.lerp(darkerColor, black, darkerAdvance * 0.3);
+  var adjustedLighter = Color.lerp(lighterColor, white, lighterAdvance);
+  var adjustedDarker = Color.lerp(darkerColor, black, darkerAdvance);
   var colors = [adjustedDarker, adjustedLighter];
   return isReversed ? colors.reversed.toList() : colors;
 }
 
-List<Color> choosetriadic(Entropy entropy) {
-  var spectrum = entropy.nextFrac();
-  var lighterAdvance = entropy.nextFrac();
-  var darkerAdvance = entropy.nextFrac();
+List<Color> triadicGradient(Entropy entropy) {
+  var spectrum1 = entropy.nextFrac();
+  var spectrum2 = (spectrum1 + 1 / 3) % 1;
+  var spectrum3 = (spectrum1 + 2 / 3) % 1;
+  var lighterAdvance = entropy.nextFrac() * 0.3;
+  var darkerAdvance = entropy.nextFrac() * 0.3;
   bool isReversed = entropy.nextBool();
-  return triadicGradient(spectrum, lighterAdvance, darkerAdvance, isReversed);
-}
 
-List<Color> triadicGradient(double spectrum, double lighterAdvance,
-    double darkerAdvance, bool isReversed) {
-  var spectrum2 = (spectrum + 1 / 3) % 1;
-  var spectrum3 = (spectrum + 2 / 3) % 1;
-  var colors = [spectrum, spectrum2, spectrum3].map(spectrumColor).toList()
+  var colors = [spectrum1, spectrum2, spectrum3].map(spectrumColor).toList()
     ..sort((col1, col2) =>
-        (col1.computeLuminance() - col2.computeLuminance()).ceil());
-  var adjustedLighter = Color.lerp(colors[2], white, lighterAdvance * 0.3);
-  var adjustedDarker = Color.lerp(colors[0], black, darkerAdvance * 0.3);
+        (col1.computeLuminance().compareTo(col2.computeLuminance())));
+  var adjustedLighter = Color.lerp(colors[2], white, lighterAdvance);
+  var adjustedDarker = Color.lerp(colors[0], black, darkerAdvance);
   var gradientColors = [adjustedLighter, colors[1], adjustedDarker];
   return isReversed ? gradientColors.reversed.toList() : gradientColors;
 }
 
-List<Color> chooseAnalogous(Entropy entropy) {
-  var spectrum = entropy.nextFrac();
-  var advance = entropy.nextFrac();
+List<Color> analgousGradient(Entropy entropy) {
+  var spectrum1 = entropy.nextFrac();
+  var advance = entropy.nextFrac() * 0.5 + 0.2;
   bool isReversed = entropy.nextBool();
-  return analgousGradient(spectrum, advance, isReversed);
-}
 
-List<Color> analgousGradient(
-    double spectrum1, double advance, bool isReversed) {
   var spectrum2 = (spectrum1 + 1 / 12) % 1;
   var spectrum3 = (spectrum1 + 2 / 12) % 1;
   var spectrum4 = (spectrum1 + 3 / 12) % 1;
@@ -79,11 +67,10 @@ List<Color> analgousGradient(
   if (colors[0].computeLuminance() > colors[3].computeLuminance()) {
     colors = colors.reversed.toList();
   }
-  var adjustedAdvance = advance * 0.5 + 0.2;
-  var adjustedDarkest = Color.lerp(colors[0], black, adjustedAdvance);
-  var adjustedDark = Color.lerp(colors[1], black, adjustedAdvance / 2);
-  var adjustedLight = Color.lerp(colors[2], white, adjustedAdvance / 2);
-  var adjustedLightest = Color.lerp(colors[3], white, adjustedAdvance);
+  var adjustedDarkest = Color.lerp(colors[0], black, advance);
+  var adjustedDark = Color.lerp(colors[1], black, advance / 2);
+  var adjustedLight = Color.lerp(colors[2], white, advance / 2);
+  var adjustedLightest = Color.lerp(colors[3], white, advance);
   var gradientColors = [
     adjustedDarkest,
     adjustedDark,
@@ -93,29 +80,23 @@ List<Color> analgousGradient(
   return isReversed ? gradientColors.reversed.toList() : gradientColors;
 }
 
-List<Color> chooseMonochrome(Entropy entropy) {
+List<Color> monochromeGradient(Entropy entropy) {
   double hue = entropy.nextFrac();
   bool isTint = entropy.nextBool();
   bool isReversed = entropy.nextBool();
-  double keyAdvance = entropy.nextFrac();
-  double neutralAdvance = entropy.nextFrac();
-  return monochromeGradient(
-      hue, isTint, isReversed, keyAdvance, neutralAdvance);
-}
+  double keyAdvance = entropy.nextFrac() * 0.3 + 0.05;
+  double neutralAdvance = entropy.nextFrac() * 0.3 + 0.05;
 
-List<Color> monochromeGradient(double hue, bool isTint, bool isReversed,
-    double keyAdvance, double neutralAdvance) {
   var keyColor = HSVColor.fromAHSV(1, hue, 1, 1).toColor();
   var contrastBrightness = isTint ? 1.0 : 0.0;
   if (isTint) {
-    keyColor.withRed((keyColor.red * 2 / 3).round())
-      ..withGreen((keyColor.green * 2 / 3).round())
-      ..withBlue((keyColor.blue * 2 / 3).round());
+    keyColor.withRed((keyColor.red / 2).round())
+      ..withGreen((keyColor.green / 2).round())
+      ..withBlue((keyColor.blue * 2).round());
   }
   var neutralColor = HSVColor.fromAHSV(1, 0, 0, contrastBrightness).toColor();
-  var keyColor2 = Color.lerp(keyColor, neutralColor, keyAdvance * 0.3 + 0.05);
-  var neutralColor2 =
-      Color.lerp(neutralColor, keyColor, keyAdvance * 0.3 + 0.05);
+  var keyColor2 = Color.lerp(keyColor, neutralColor, keyAdvance);
+  var neutralColor2 = Color.lerp(neutralColor, keyColor, neutralAdvance);
   var colors = [keyColor2, neutralColor2];
   return isReversed ? colors.reversed.toList() : colors;
 }
