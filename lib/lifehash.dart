605dc19d705104cd3021d72aa14c91a69bd73045
Dominik Spicher <dominikspicher@gmail.com>
2020-11-14 13:34:32 +0100

    lifehash: add top-level lifehash function

--- lib/lifehash.dart
+++ lib/lifehash.dart
@@ -1,14 +1,39 @@
+import 'dart:ui';
 
-import 'dart:async';
+import 'package:crypto/crypto.dart';
+import 'package:lifehash/color.dart';
+import 'package:lifehash/conway.dart';
+import 'package:lifehash/entropy.dart';
+import 'package:lifehash/symmetry.dart';
 
-import 'package:flutter/services.dart';
+List<List<double>> grayscale(Grid grid) {
+  var states = converge(grid, 150);
+  return combineStates(states);
+}
 
-class Lifehash {
-  static const MethodChannel _channel =
-      const MethodChannel('lifehash');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+List<List<double>> combineStates(List<List<List<int>>> states) {
+  var size = states[0].length;
+  var result = List.generate(size, (_) => List.generate(size, (_) => 0.0));
+  for (int i = 0; i < states.length; i++) {
+    for (int x = 0; x < size; x++) {
+      for (int y = 0; y < size; y++) {
+        if (states[i][x][y] > 0) {
+          result[x][y] = (i + 1) / states.length;
+        }
+      }
+    }
   }
+  return result;
+}
+
+List<List<Color>> lifehash(List<int> bytes) {
+  var digest = sha256.convert(bytes);
+  var grayValues = grayscale(Grid.digest(digest));
+  var entropy = Entropy.fromBytes(digest.bytes);
+  var colors = chooseGradient(entropy);
+  var symmetric =
+      entropy.nextBool() ? snowflake(grayValues) : pinwheel(grayValues);
+  return symmetric
+      .map((l) => l.map((f) => lerpMany(colors, f)).toList())
+      .toList();
 }
--- test/lifehash_test.dart
+++ test/lifehash_test.dart
@@ -1,23 +1,22 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:lifehash/conway.dart';
 import 'package:lifehash/lifehash.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('lifehash');
-
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
+  group('grayscale', () {
+    test('returns correctly for blinker', () {
+      var osc = Grid.zero(5);
+      osc.set(2, 1, true);
+      osc.set(2, 2, true);
+      osc.set(2, 3, true);
+      var gray = grayscale(osc);
+      expect(gray, [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0.5, 1, 0.5, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+      ]);
     });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await Lifehash.platformVersion, '42');
   });
 }
